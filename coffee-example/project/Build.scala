dee1147a6be621f7c06481f5a0e9d5d6dd501aab
unknown <skprasadu@yahoo.com>
2013-03-22 08:17:34 -0400

    removed few files

--- coffee-example/project/Build.scala
+++ coffee-example/project/Build.scala
@@ -9,7 +9,7 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     jdbc,
-    anorm,
+    "org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP9" % "test",
     "com.typesafe" % "slick_2.10.0-RC1" % "0.11.2",
     "org.slf4j" % "slf4j-nop" % "1.6.4",
     "com.h2database" % "h2" % "1.3.166",
