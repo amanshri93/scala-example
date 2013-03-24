20a74e1378f46d8def7390a93d3ace7ac72ea5ac
unknown <skprasadu@yahoo.com>
2013-03-24 09:07:19 -0400

    changed the build.scala

--- coffee-example/project/Build.scala
+++ coffee-example/project/Build.scala
@@ -9,15 +9,14 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     jdbc,
-    "org.scalamock" %% "scalamock-scalatest-support" % "3.0" % "test",
-    "com.typesafe" % "slick_2.10.0-RC1" % "0.11.2",
+    "com.typesafe.slick" %% "slick" % "1.0.0",
     "org.slf4j" % "slf4j-nop" % "1.6.6",
     "org.slf4j" % "slf4j-api" % "1.6.6",
     "com.h2database" % "h2" % "1.3.166",
-    "org.xerial" % "sqlite-jdbc" % "3.6.20")
+    "org.xerial" % "sqlite-jdbc" % "3.6.20",
+    "org.scalamock" %% "scalamock-scalatest-support" % "3.0" % "test")
 
-  val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
-    //scalaVersion := "2.10.1"
+  val main = play.Project(appName, appVersion, appDependencies).settings( 
+      // Add your own project settings here      
   )
-
 }
