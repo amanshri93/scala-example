ef79450a16da3a5fbe685f8359bf0d6d1e09b397
unknown <skprasadu@yahoo.com>
2013-03-25 08:35:42 -0400

    modified coffee-example with route test

--- coffee-example/test/controllers/CoffeesControllerTest.scala
+++ coffee-example/test/controllers/CoffeesControllerTest.scala
@@ -10,14 +10,30 @@ import dal.CoffeeComponent
 class CoffeesControllerTest extends FunSpec with ShouldMatchers with MockFactory {
   describe("Coffee Controller with Mock test") {
 
-    it("should delete a coffee record with assert on status") {
+    it("should delete a coffee record with assert on status as SEE_OTHER") {
       running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
         val mockComponent = mock[CoffeeComponent]
         (mockComponent.delete _).expects("Columbian") returning (1) twice
         val controller = new CoffeesController(mockComponent)
-        mockComponent.delete("Columbian") should equal (1)
+        mockComponent.delete("Columbian") should equal(1)
         val result = controller.delete("Columbian")(FakeRequest())
-        status(result) should equal (SEE_OTHER)
+        status(result) should equal(SEE_OTHER)
+      }
+    }
+
+    it("POST /coffees/Colombian/delete should return SEE_OTHER") {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+
+        val Some(result) = route(FakeRequest(POST, "/coffees/Colombian/delete"))
+        status(result) should equal(SEE_OTHER)
+      }
+    }
+
+    it("POST wrong url /coffees/Colombian/delete1 should return None") {
+      running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+
+        val result = route(FakeRequest(POST, "/coffees/Colombian/delete1"))
+        result should equal(None)
       }
     }
   }
