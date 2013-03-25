9ab184927178962633c1ff661635bdbe99ab929d
unknown <skprasadu@yahoo.com>
2013-03-25 10:29:33 -0400

    added dependency for selenium test

--- coffee-example/project/Build.scala
+++ coffee-example/project/Build.scala
@@ -14,7 +14,8 @@ object ApplicationBuild extends Build {
     "org.slf4j" % "slf4j-api" % "1.6.6",
     "com.h2database" % "h2" % "1.3.166",
     "org.xerial" % "sqlite-jdbc" % "3.6.20",
-    "org.scalamock" %% "scalamock-scalatest-support" % "3.0" % "test")
+    "org.scalamock" %% "scalamock-scalatest-support" % "3.0" % "test",
+    "org.seleniumhq.selenium" % "selenium-java" % "2.31.0" )
 
   val main = play.Project(appName, appVersion, appDependencies).settings( 
       // Add your own project settings here      
